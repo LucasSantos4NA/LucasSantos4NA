9bf08f98476813b16ddcdff18d6b4e8c7a9ebe7e
Lucas Santos <35945805-lucassantos5591@users.noreply.replit.com>
2024-10-09 22:49:53 +0000

    refactor: separando cada função do controller

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 // Substitua pela sua string de conexão do Render.com
 const connectionString =
-  "postgresql://lucassantos4na_user:nI0BqlYMQaPJu0Iy0AvfFIQjhGhwc71H@dpg-cr7qskd6l47c73bq6oog-a.oregon-postgres.render.com/lucassantos4na";
+  "postgresql://backend_ugaz_user:87KlrSzD5GQ0M8KBs5SaJ0bJgGj5112g@dpg-cs2rppbv2p9s738nq950-a.oregon-postgres.render.com/backend_ugaz";
 
 const pool = new Pool({
   connectionString,
